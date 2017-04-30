6c9f4023888d11ee5cfd08d100f80b78cbf18c41
ord4 <ord4@zips.uakron.edu>
2017-04-29 21:41:51 -0400

    Starting stack

--- stack.hpp
+++ stack.hpp
@@ -7,14 +7,58 @@
 #ifndef STACK_HPP
 #define STACK_HPP
 
-#include "test.hpp"
-
 #include <stack>
-
-
-// TODO: You *must* implement a stack.
+/*// You *must* implement a stack.
 template<typename T>
-using Stack = std::stack<T>;
+using Stack = std::stack<T>;*/
 
+template <class T> class Stack{
+	private:
+		struct Node{
+			T data;
+			T *nextPtr;
+		};
+		struct Node *top;
+		int stackSize;
+
+	public:
+		Stack(){
+			top = nullptr;
+			stackSize = 0;
+		}
+
+		bool isEmpty(){
+			if(top == nullptr){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+
+		std::size_t size() const{
+			return stackSize;
+		}
+
+		template<class T> top(){
+			return top->data;
+		}
+
+		template<class T>* top(){
+			return top;
+		}
+
+		void push_front(T value){
+			stackSize++;
+
+			struct Node *newNode;
+			newNode = new Node;
+
+			newNode->data = value;
+			newNode->nextPtr = nullptr;
+
+			if()
+		}
+}
 
 #endif
