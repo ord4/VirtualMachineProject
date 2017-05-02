e26e499ea506049c7af23585a98a604697483c12
ord4 <ord4@zips.uakron.edu>
2017-05-02 09:33:02 -0400

    Vector complete

--- vector.hpp
+++ vector.hpp
@@ -76,6 +76,15 @@ template <class T> class Vec{
 		iterator limit;
 };
 
+template < class T>
+Vec<T>& Vec<T>::operator=(const Vec &rhs){
+	if(&rhs != this){
+		uncreate();
+		create(rhs.begin(), rhs.end());
+	}
+	return *this;
+}
+
 template <class T>
 void Vec<T>::create(){
 	data = avail = limit = 0;
@@ -87,5 +96,54 @@ void Vec<T>::create(size_type n, const T& val){
 	limit = avail = data + n;
 	uninitialized_fill(data, limit, val);
 }
+
+template <class T>
+void Vec<T>::create(const_iterator i, const_iterator j){
+	data = alloc.allocate(j - i);
+	limit = avail = uninitialized_copy(i, j, data);
+}
+
+template <class T>
+void Vec<T>::uncreate(){
+	if(data){
+		iterator it = avail;
+		while(it != data){
+			alloc.destroy(--it);
+		}
+		alloc.deallocate(data, limit - data);
+	}
+	data = limit = avail = 0;
+}
+
+template <class T>
+void Vec<T>::grow(){
+	size_type new_size = max(2 * (limit - data), ptrdiff_t(1));
+	iterator new_data = alloc.allocate(new_size);
+	iterator new_avail = uninitialized_copy(data, avail, new_data);
+	uncreate();
+	data = new_data;
+	avail = new_avail;
+	limit = data + new_size;
+}
+
+template <class T>
+void Vec<T>::unchecked_append(const T& val){
+	alloc.construct(avail++, val);
+}
+
+
+template <class T> allocator{
+	public:
+		T* allocate(size_t);
+		void deallocate(T*, size_t);
+		void construct(T*, const T&);
+		void destroy(T*);
+};
+
+template <class Out, class T>
+void unitialized_fill(Out, Out, const T&);
+
+template <class In, class Out>
+Out uninitialized_copy(In, In, Out);
 
 #endif
